7a8356f5815d6957bf1ee15d3439f5ee18ce767a
viktoriamay <alterkelly@mail.ru>
2023-04-13 20:23:24 +0300

    Add MainPage

---
+++ src/pages/MainPage/MainPage.jsx
@@ -0,0 +1,13 @@
+import { HomepageInfo } from "../../components/HomepageInfo/HomepageInfo";
+import "./MainPage.scss";
+
+export const MainPage = () => {
+  return (
+    <div className="main_page">
+     <HomepageInfo />
+      <div className="main_page__categories">
+
+      </div>
+    </div>
+  );
+};
