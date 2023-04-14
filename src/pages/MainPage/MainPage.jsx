175dbed5d0d259e06e9e93f2563bdc92d5609df8
viktoriamay <alterkelly@mail.ru>
2023-04-14 23:41:56 +0300

    Add to MainPage homepage components

--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -1,13 +1,16 @@
-import { HomepageInfo } from "../../components/HomepageInfo/HomepageInfo";
+import { HomepageCategories } from "../../components/Homepage/HomepageCategories/HomepageCategories";
+import { HomepageCharacters } from "../../components/Homepage/HomepageCharacters/HomepageCharacters";
+import { HomepageInfo } from "../../components/Homepage/HomepageInfo/HomepageInfo";
+import { HomepageNews } from "../../components/Homepage/HomepageNews/HomepageNews";
 import "./MainPage.scss";
 
 export const MainPage = () => {
   return (
     <div className="main_page">
-     <HomepageInfo />
-      <div className="main_page__categories">
-
-      </div>
+      <HomepageInfo />
+      <HomepageCategories />
+      <HomepageCharacters />
+      <HomepageNews />
     </div>
   );
 };
