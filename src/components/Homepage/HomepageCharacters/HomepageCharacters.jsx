b740c95688c83abaf7aac52aec9f560b5aab34ed
viktoriamay <alterkelly@mail.ru>
2023-04-14 23:44:40 +0300

    Add HomepageCharacters component

---
+++ src/components/Homepage/HomepageCharacters/HomepageCharacters.jsx
@@ -0,0 +1,74 @@
+import "./HomepageCharacters.scss";
+
+export const HomepageCharacters = () => {
+  return (
+    <div className="container">
+      <div className="homepage_characters">
+    <div className="circles"></div>
+        <h2 className="homepage__title">Главные персонажи</h2>
+        <div className="homepage_characters__cards">
+        <div className="homepage_characters__card">
+            <div className="homepage_characters__card_img_wrapper">
+              <img
+                src="https://www.nme.com/wp-content/uploads/2022/10/Rick_And_Morty_Portal_Gun_JuRicksic_Mort.jpg"
+                alt=""
+                className="homepage_characters__card_img"
+              />
+            </div>
+            <div className="homepage_characters__info_wrapper">
+              <p className="homepage_characters__card_name">Rick Sanchez</p>
+              <span className="homepage_characters__card_about">Human</span>
+              <span className="homepage_characters__card_about">Male</span>
+              <span className="homepage_characters__card_about">Earth</span>
+            </div>
+          </div>
+          <div className="homepage_characters__card">
+            <div className="homepage_characters__card_img_wrapper">
+              <img
+                src="https://www.nme.com/wp-content/uploads/2022/10/Rick_And_Morty_Portal_Gun_JuRicksic_Mort.jpg"
+                alt=""
+                className="homepage_characters__card_img"
+              />
+            </div>
+            <div className="homepage_characters__info_wrapper">
+              <p className="homepage_characters__card_name">Rick Sanchez</p>
+              <span className="homepage_characters__card_about">Human</span>
+              <span className="homepage_characters__card_about">Male</span>
+              <span className="homepage_characters__card_about">Earth</span>
+            </div>
+          </div>
+          <div className="homepage_characters__card">
+            <div className="homepage_characters__card_img_wrapper">
+              <img
+                src="https://www.nme.com/wp-content/uploads/2022/10/Rick_And_Morty_Portal_Gun_JuRicksic_Mort.jpg"
+                alt=""
+                className="homepage_characters__card_img"
+              />
+            </div>
+            <div className="homepage_characters__info_wrapper">
+              <p className="homepage_characters__card_name">Rick Sanchez</p>
+              <span className="homepage_characters__card_about">Human</span>
+              <span className="homepage_characters__card_about">Male</span>
+              <span className="homepage_characters__card_about">Earth</span>
+            </div>
+          </div>
+          <div className="homepage_characters__card">
+            <div className="homepage_characters__card_img_wrapper">
+              <img
+                src="https://www.nme.com/wp-content/uploads/2022/10/Rick_And_Morty_Portal_Gun_JuRicksic_Mort.jpg"
+                alt=""
+                className="homepage_characters__card_img"
+              />
+            </div>
+            <div className="homepage_characters__info_wrapper">
+              <p className="homepage_characters__card_name">Rick Sanchez</p>
+              <span className="homepage_characters__card_about">Human</span>
+              <span className="homepage_characters__card_about">Male</span>
+              <span className="homepage_characters__card_about">Earth</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
