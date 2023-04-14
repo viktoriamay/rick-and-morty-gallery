c66c8f0a985dcf76ad83a3903c6f8c6fe4ed8a72
viktoriamay <alterkelly@mail.ru>
2023-04-14 23:44:59 +0300

    Add HomepageNews component

---
+++ src/components/Homepage/HomepageNews/HomepageNews.jsx
@@ -0,0 +1,64 @@
+import "./HomepageNews.scss";
+
+export const HomepageNews = () => {
+  return (
+    <div className="container">
+      <div className="homepage_news">
+        <h2 className="homepage__title">News</h2>
+        <div className="homepage_news__cards">
+          <div className="homepage_news__card">
+            <div className="homepage_news__card_img_wrapper">
+              <img
+                src="https://static1.colliderimages.com/wordpress/wp-content/uploads/2022/08/Rick--Morty-Season-6EWKSF-feature.jpg"
+                alt=""
+                className="homepage_news__card_img"
+              />
+            </div>
+            <div className="homepage_news__card_info__wrapper">
+              <p className="homepage_news__card_title">
+                News about Rick Sanchez
+              </p>
+              <span className="homepage_news__card_text">News about Human</span>
+              <span className="homepage_news__card_text">News about Male</span>
+              <span className="homepage_news__card_button">Button</span>
+            </div>
+          </div>
+          <div className="homepage_news__card">
+            <div className="homepage_news__card_img_wrapper">
+              <img
+                src="https://static1.colliderimages.com/wordpress/wp-content/uploads/2022/08/Rick--Morty-Season-6EWKSF-feature.jpg"
+                alt=""
+                className="homepage_news__card_img"
+              />
+            </div>
+            <div className="homepage_news__card_info__wrapper">
+              <p className="homepage_news__card_title">
+                News about Rick Sanchez
+              </p>
+              <span className="homepage_news__card_text">News about Human</span>
+              <span className="homepage_news__card_text">News about Male</span>
+              <span className="homepage_news__card_button">Button</span>
+            </div>
+          </div>
+          <div className="homepage_news__card">
+            <div className="homepage_news__card_img_wrapper">
+              <img
+                src="https://static1.colliderimages.com/wordpress/wp-content/uploads/2022/08/Rick--Morty-Season-6EWKSF-feature.jpg"
+                alt=""
+                className="homepage_news__card_img"
+              />
+            </div>
+            <div className="homepage_news__card_info__wrapper">
+              <p className="homepage_news__card_title">
+                News about Rick Sanchez
+              </p>
+              <span className="homepage_news__card_text">News about Human</span>
+              <span className="homepage_news__card_text">News about Male</span>
+              <span className="homepage_news__card_button">Button</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
