bf9f47cd09c74fd780c3dbb06182f2150ec7c496
viktoriamay <alterkelly@mail.ru>
2023-04-17 23:09:02 +0300

    Edit ExplorePage

--- src/pages/ExplorePage/ExplorePage.jsx
+++ src/pages/ExplorePage/ExplorePage.jsx
@@ -1,36 +1,48 @@
-import { Link, NavLink, Outlet, Route, Routes } from "react-router-dom";
+import { Link, NavLink, Outlet, Route, Routes, useLocation} from "react-router-dom";
 import { SearchCharactersPage } from "./SearchCharactersPage/SearchCharactersPage";
 import { SearchLocationsPage } from "./SearchLocationsPage/SearchLocationsPage";
 import { SearchEpisodesPage } from "./SearchEpisodesPage/SearchEpisodesPage";
-import './ExplorePage.scss'
-export const ExplorePage = ({children}) => {
+import "./ExplorePage.scss";
+import { Search } from "../../components/Search/Search";
+export const ExplorePage = () => {
+
+  const location = useLocation();
+
+  const res = location.pathname.split('/');
+  const w = res[res.length - 1];
+  const j = w.replace(w[0], w[0].toUpperCase())
+  console.log(j)
+
+
   return (
-    <div>
-    HUUIFVFNVIFN
-      <NavLink to={"/rick-and-morty-gallery/explore/characters"}>CHAR</NavLink>
-      <NavLink to={"/rick-and-morty-gallery/explore/locations"}>LOC</NavLink>
-      <NavLink to={"/rick-and-morty-gallery/explore/episodes"}>EPI</NavLink>
-      {children}
-      <Outlet />
-      {/* <Routes>
-        <Route
-          path="/characters"
-          element={<SearchCharactersPage />}
-        />
-        <Route
-          path="/locations"
-          element={<SearchLocationsPage />}
-        />
-        <Route
-          path="/episodes"
-          element={<SearchEpisodesPage />}
-        />
-      </Routes> */}
-      {/* <SearchCharactersPage />
-<SearchLocationsPage />
-<SearchEpisodesPage /> */}
+    <div className="explore_page">
+      <div className="container">
+        <div className="explore_page__container">
+        {/* <h2>{j}</h2> */}
+          <div className="explore_page__navlink_container">
+            <NavLink
+              className="explore_page__navlink"
+              to={"/rick-and-morty-gallery/explore/characters"}
+            >
+              Персонажи
+            </NavLink>
+            <NavLink
+              className="explore_page__navlink"
+              to={"/rick-and-morty-gallery/explore/locations"}
+            >
+              Локации
+            </NavLink>
+            <NavLink
+              className="explore_page__navlink"
+              to={"/rick-and-morty-gallery/explore/episodes"}
+            >
+              Эпизоды
+            </NavLink>
+          </div>
+<Search />
+        </div>
+        <Outlet />
+      </div>
     </div>
   );
 };
-
-
