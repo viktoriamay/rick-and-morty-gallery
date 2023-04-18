f268633a8011600585fe68b56222a47508e1ee25
viktoriamay <alterkelly@mail.ru>
2023-04-19 02:09:42 +0300

    Add Search function

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,12 +1,22 @@
 import { TfiSearch } from "react-icons/tfi";
 import "./Search.scss";
+import { useContext } from "react";
+import { GalleryContext } from "../../utils/context/GalleryContext";
 
 export const Search = () => {
+
+  const {updatePageNumber, setSearchQuery} = useContext(GalleryContext);
+
+  const searchRequest = (e => e.preventDefault())
+
   return (
     <form className="search__form">
       <div className="search__form_container">
-        <input className="search__input" type="text" placeholder="Поиск" />
-        <button className="search__button">
+        <input className="search__input" type="text" placeholder="Поиск" onChange={(e) => {
+    updatePageNumber(1);
+    setSearchQuery(e.target.value);
+  }} />
+        <button className="search__button" onClick={searchRequest}>
           <TfiSearch />
         </button>
       </div>
