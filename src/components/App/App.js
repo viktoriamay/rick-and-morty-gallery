29b4d8f69fc6098cc0ca40b1d7b4a062db4dc0f7
viktoriamay <alterkelly@mail.ru>
2023-04-19 02:12:31 +0300

    Add get characters and search characters functions

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -9,41 +9,93 @@ import { NewsPage } from "../../pages/NewsPage/NewsPage";
 import { SearchCharactersPage } from "../../pages/ExplorePage/SearchCharactersPage/SearchCharactersPage";
 import { SearchLocationsPage } from "../../pages/ExplorePage/SearchLocationsPage/SearchLocationsPage";
 import { SearchEpisodesPage } from "../../pages/ExplorePage/SearchEpisodesPage/SearchEpisodesPage";
+import { useEffect, useState } from "react";
+import RickMortyApi from "../../utils/api/rickMortyApi";
+import { GalleryContext } from "../../utils/context/GalleryContext";
+import useDebounce from "../../hooks/useDebounce";
 
 function App() {
+  const [characters, setCharacters] = useState([]);
+
+  const [pageNumber, updatePageNumber] = useState(1);
+  const [searchQuery, setSearchQuery] = useState("");
+
+  // useEffect(() => {
+  //   RickMortyApi.getCharactersList().then((characters) => {
+  //     setCharacters(characters.results);
+  //   });
+  // }, []);
+
+  const debounceSearchQuery = useDebounce(searchQuery, 1000);
+  
+  useEffect(() => {
+    // const request = () =>
+
+    RickMortyApi.getCharacters(pageNumber)
+      .then((characters) => setCharacters(characters.results))
+    // request()
+    // .then(( pageNumber) => {
+    // setSearchQuery(searchQuery);
+    // updatePageNumber(pageNumber)
+    // }
+    // );
+  }, [pageNumber]);
+
+
+  const filteredCharactersRequest = () => {
+
+    const searchQuery = debounceSearchQuery;
+
+    RickMortyApi.searchCharacters(searchQuery)
+    .then((filteredCharacters) => setCharacters(filteredCharacters.results));
+
+
+  }
+  useEffect(() => {
+    filteredCharactersRequest();
+  }, [debounceSearchQuery]);
+
+  console.log(pageNumber);
+  const valueContextProvider = { characters, setSearchQuery, updatePageNumber };
+
   return (
-    <div className="App">
-      <Header />
+    <GalleryContext.Provider value={valueContextProvider}>
+      <div className="App">
+        <Header />
 
-      <Routes>
-        <Route path="/rick-and-morty-gallery" element={<MainPage />} />
-      </Routes>
-
-      <div className="main">
         <Routes>
-          <Route
-            path="/rick-and-morty-gallery/explore"
-            element={
-              <Navigate to="/rick-and-morty-gallery/explore/characters" />
-            }
-          />
-          <Route
-            path="/rick-and-morty-gallery/explore/*"
-            element={<ExplorePage />}
-          >
-            <Route path="characters" element={<SearchCharactersPage />} />
-            <Route path="locations" element={<SearchLocationsPage />} />
-            <Route path="episodes" element={<SearchEpisodesPage />} />
-          </Route>
-          <Route
-            path="/rick-and-morty-gallery/statistics"
-            element={<StatisticsPage />}
-          />
-          <Route path="/rick-and-morty-gallery/news" element={<NewsPage />} />
+          <Route path="/rick-and-morty-gallery" element={<MainPage />} />
         </Routes>
+
+        <div className="main">
+          <button onClick={() => updatePageNumber(pageNumber + 1)}>
+            {pageNumber}
+          </button>
+          <Routes>
+            <Route
+              path="/rick-and-morty-gallery/explore"
+              element={
+                <Navigate to="/rick-and-morty-gallery/explore/characters" />
+              }
+            />
+            <Route
+              path="/rick-and-morty-gallery/explore/*"
+              element={<ExplorePage />}
+            >
+              <Route path="characters" element={<SearchCharactersPage />} />
+              <Route path="locations" element={<SearchLocationsPage />} />
+              <Route path="episodes" element={<SearchEpisodesPage />} />
+            </Route>
+            <Route
+              path="/rick-and-morty-gallery/statistics"
+              element={<StatisticsPage />}
+            />
+            <Route path="/rick-and-morty-gallery/news" element={<NewsPage />} />
+          </Routes>
+        </div>
+        <Footer />
       </div>
-      <Footer />
-    </div>
+    </GalleryContext.Provider>
   );
 }
 
