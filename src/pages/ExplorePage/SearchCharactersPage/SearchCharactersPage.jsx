15beec694ef437da33a3ded45c748ab6b5ce704c
viktoriamay <alterkelly@mail.ru>
2023-04-19 22:05:42 +0300

    Add Pagination to SearchCharactersPage

--- src/pages/ExplorePage/SearchCharactersPage/SearchCharactersPage.jsx
+++ src/pages/ExplorePage/SearchCharactersPage/SearchCharactersPage.jsx
@@ -2,17 +2,30 @@ import { useContext, useEffect, useState } from "react";
 import { CharacterCard } from "../../../components/CharacterCard/CharacterCard";
 import { Filter } from "../../../components/Filter/Filter";
 import { GalleryContext } from "../../../utils/context/GalleryContext";
+import { Pagination } from "../../../components/Pagination/Pagination";
 
-export const SearchCharactersPage = () => { 
-
-  const {characters} = useContext(GalleryContext)
+export const SearchCharactersPage = () => {
+  const { characters, info, pageNumber, updatePageNumber } =
+    useContext(GalleryContext);
 
   return (
+    <div>
+
     <div className="scp">
-      <div><Filter/> </div>
-      <div className="characters_cards__container">
-      {characters.map((character) => <CharacterCard character={character} key={character.id} /> )}
+      <div>
+        <Filter />
       </div>
+      <div className="characters_cards__container">
+        {characters.map((character) => (
+          <CharacterCard character={character} key={character.id} />
+        ))}
+      </div>
+    </div>
+      <Pagination
+        info={info}
+        pageNumber={pageNumber}
+        updatePageNumber={updatePageNumber}
+      />
     </div>
   );
 };
--- src/pages/ExplorePage/SearchEpisodesPage/SearchEpisodesPage.jsx
+++ src/pages/ExplorePage/SearchEpisodesPage/SearchEpisodesPage.jsx
@@ -1,9 +1,12 @@
+import { Pagination } from "antd"
 
 
 export const SearchEpisodesPage = () => {
   return (
-    <div>
+    <div style={{backgroundColor: 
+    "white"}}>
        SearchEpisodesPage
+       <Pagination defaultCurrent={6} total={500} fontSize='30px' />
     </div>
   )
 }
