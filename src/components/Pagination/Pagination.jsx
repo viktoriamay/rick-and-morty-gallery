7e0f42d85ced95589af5c7ebb90e37564ff2f973
viktoriamay <alterkelly@mail.ru>
2023-04-28 19:15:40 +0300

    Add LocationPage

--- src/components/Pagination/Pagination.jsx
+++ src/components/Pagination/Pagination.jsx
@@ -1,20 +1,23 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useContext } from "react";
 import ReactPaginate from "react-paginate";
-import './Pagination.scss'
+import "./Pagination.scss";
+import { GalleryContext } from "../../utils/context/GalleryContext";
 
-export const Pagination = ({ pageNumber, info, updatePageNumber }) => {
+export const Pagination = () => {
   let pageChange = (data) => {
     updatePageNumber(data.selected + 1);
   };
 
-  const [width, setWidth] = useState(window.innerWidth);
-  const updateDimensions = () => {
-    setWidth(window.innerWidth);
-  };
-  useEffect(() => {
-    window.addEventListener("resize", updateDimensions);
-    return () => window.removeEventListener("resize", updateDimensions);
-  }, []);
+  const { info, pageNumber, updatePageNumber, characters } =
+    useContext(GalleryContext);
+  // const [width, setWidth] = useState(window.innerWidth);
+  // const updateDimensions = () => {
+  //   setWidth(window.innerWidth);
+  // };
+  // useEffect(() => {
+  //   window.addEventListener("resize", updateDimensions);
+  //   return () => window.removeEventListener("resize", updateDimensions);
+  // }, []);
 
   // const pageCount = Math.ceil(info?.count / 20);
 
@@ -22,29 +25,31 @@ export const Pagination = ({ pageNumber, info, updatePageNumber }) => {
 
   return (
     <>
-      <ReactPaginate
-        className="pagination"
-        nextLabel="Следующая"
-        previousLabel="Предыдущая"
-        previousClassName="pagination__nav_button"
-        nextClassName="pagination__nav_button"
-        activeClassName="active"
-        pageClassName="pagination__page_button"
-        pageLinkClassName="pagination__page_link_button"
-        // forcePage={pageNumber === 1 ? 0 : pageNumber - 1}
-        forcePage={pageNumber <= 1 ? 0 : pageNumber - 1}
-        // forcePage={pageNumber - 1}
-        marginPagesDisplayed={width < 576 ? 1 : 2}
-        pageRangeDisplayed={width < 576 ? 1 : 2}
+      {info.count > 20 && (
+        <ReactPaginate
+          className="pagination"
+          nextLabel="Следующая"
+          previousLabel="Предыдущая"
+          previousClassName="pagination__nav_button"
+          nextClassName="pagination__nav_button"
+          activeClassName="active"
+          pageClassName="pagination__page_button"
+          pageLinkClassName="pagination__page_link_button"
+          // forcePage={pageNumber === 1 ? 0 : pageNumber - 1}
+          forcePage={pageNumber <= 1 ? 0 : pageNumber - 1}
+          // forcePage={pageNumber - 1}
+          // marginPagesDisplayed={width < 576 ? 1 : 2}
+          // pageRangeDisplayed={width < 576 ? 1 : 2}
 
-        pageCount={info?.pages}
-        // pageCount={pageCount}
-        onPageChange={pageChange}
+          pageCount={info?.pages}
+          // pageCount={pageCount}
+          onPageChange={pageChange}
 
-        // pageRangeDisplayed={1}
-        // marginPagesDisplayed={1}
-        //.... other props here
-      />
+          // pageRangeDisplayed={1}
+          // marginPagesDisplayed={1}
+          //.... other props here
+        />
+      )}
     </>
   );
 };
--- src/pages/ExplorePage/SearchLocationsPage/SearchLocationsPage.jsx
+++ src/pages/ExplorePage/SearchLocationsPage/SearchLocationsPage.jsx
@@ -1,9 +1,23 @@
+import { useEffect, useState } from "react"
+import { LocationCard } from "../../../components/LocationCard/LocationCard"
+import RickMortyApi from "../../../utils/api/rickMortyApi";
 
 
 export const SearchLocationsPage = () => {
+
+  const [locations, setLocations] = useState([])
+
+  console.log({locations});
+  useEffect(() => {
+    RickMortyApi.getLocations().then((data) => setLocations(data.results))
+    
+  }, []);
+
+
   return (
     <div>
-       SearchLocationsPage
+    {locations.map((location) => <LocationCard location={location} />)}
+       
     </div>
   )
 }
