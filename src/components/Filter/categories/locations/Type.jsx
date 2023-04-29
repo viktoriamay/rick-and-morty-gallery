0fcfe4218867a27d2b2b625d1bae8955b966ee7a
viktoriamay <alterkelly@mail.ru>
2023-04-30 01:41:08 +0300

    Add Type option

---
+++ src/components/Filter/categories/locations/Type.jsx
@@ -0,0 +1,34 @@
+import { useContext } from "react";
+import { Accordion } from "../../../Accordion/Accordion"
+import { FilterOptionBTN } from "../../FilterOptionBTN"
+import { GalleryContext } from "../../../../utils/context/GalleryContext";
+
+
+export const Type = () => {
+
+  const {
+    pageNumber,
+    updatePageNumber,
+    updateStatus,
+    updateGender,
+    updateSpecies,
+  } = useContext(GalleryContext);
+  
+  let type = ["Planet", "Cluster", "Space station", "Microverse", "TV", "Resort", "Fantasy town", "Dream", "Dimension", "Menagerie", "Game", "Custom", "Unknown"];
+  return (
+    <>
+       <Accordion title="Тип">
+        {type.map((option, index) => (
+          <FilterOptionBTN
+            index={index}
+            name="gender"
+            key={`gender-${index}`}
+            optionName={option}
+            task={updateGender}
+
+          />
+        ))}
+      </Accordion>
+    </>
+  )
+}
