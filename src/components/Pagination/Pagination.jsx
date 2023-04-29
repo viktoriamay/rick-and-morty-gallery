7f9e496511fc5a8da9bfe08561b26059f31aae71
viktoriamay <alterkelly@mail.ru>
2023-04-30 01:43:10 +0300

    Edit Pagination component (for universal use in SearchPages)

--- src/components/Pagination/Pagination.jsx
+++ src/components/Pagination/Pagination.jsx
@@ -2,14 +2,52 @@ import React, { useState, useEffect, useContext } from "react";
 import ReactPaginate from "react-paginate";
 import "./Pagination.scss";
 import { GalleryContext } from "../../utils/context/GalleryContext";
+import { useLocation } from "react-router-dom";
 
 export const Pagination = () => {
-  let pageChange = (data) => {
-    updatePageNumber(data.selected + 1);
+  const location = useLocation();
+  const pageChange = (data) => {
+    if (location.pathname === "/rick-and-morty-gallery/explore/characters") {
+      updatePageNumberCharacters(data.selected + 1);
+    } else if (
+      location.pathname === "/rick-and-morty-gallery/explore/locations"
+    ) {
+      updatePageNumberLocations(data.selected + 1);
+    }
   };
 
-  const { info, pageNumber, updatePageNumber, characters } =
-    useContext(GalleryContext);
+  const paginationCount = (location) => {
+    switch (location) {
+      case "/rick-and-morty-gallery/explore/characters":
+        return infoCharacters?.pages;
+      case "/rick-and-morty-gallery/explore/locations":
+        return infoLocations?.pages;
+
+      default:
+        return null;
+    }
+  };
+
+  const paginationCurrentPage = (location) => {
+    switch (location) {
+      case "/rick-and-morty-gallery/explore/characters":
+        return pageNumberCharacters - 1;
+      case "/rick-and-morty-gallery/explore/locations":
+        return pageNumberLocations - 1;
+
+      default:
+        return null;
+    }
+  };
+
+  const {
+    infoCharacters,
+    pageNumberCharacters,
+    pageNumberLocations,
+    updatePageNumberLocations,
+    updatePageNumberCharacters,
+    infoLocations,
+  } = useContext(GalleryContext);
   // const [width, setWidth] = useState(window.innerWidth);
   // const updateDimensions = () => {
   //   setWidth(window.innerWidth);
@@ -25,7 +63,7 @@ export const Pagination = () => {
 
   return (
     <>
-      {info.count > 20 && (
+      {infoCharacters.count > 20 && (
         <ReactPaginate
           className="pagination"
           nextLabel="Следующая"
@@ -35,19 +73,11 @@ export const Pagination = () => {
           activeClassName="active"
           pageClassName="pagination__page_button"
           pageLinkClassName="pagination__page_link_button"
-          // forcePage={pageNumber === 1 ? 0 : pageNumber - 1}
-          forcePage={pageNumber <= 1 ? 0 : pageNumber - 1}
-          // forcePage={pageNumber - 1}
           // marginPagesDisplayed={width < 576 ? 1 : 2}
           // pageRangeDisplayed={width < 576 ? 1 : 2}
-
-          pageCount={info?.pages}
-          // pageCount={pageCount}
+          forcePage={paginationCurrentPage(location.pathname)}
+          pageCount={paginationCount(location.pathname)}
           onPageChange={pageChange}
-
-          // pageRangeDisplayed={1}
-          // marginPagesDisplayed={1}
-          //.... other props here
         />
       )}
     </>
