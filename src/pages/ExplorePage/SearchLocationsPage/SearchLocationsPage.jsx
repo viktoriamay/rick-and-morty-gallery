da20c05178f54c73343d3bb7997c24f6d150338c
viktoriamay <alterkelly@mail.ru>
2023-04-30 01:47:07 +0300

    Add SearchLocationsPage

--- src/pages/ExplorePage/SearchLocationsPage/SearchLocationsPage.jsx
+++ src/pages/ExplorePage/SearchLocationsPage/SearchLocationsPage.jsx
@@ -1,23 +1,35 @@
-import { useEffect, useState } from "react"
-import { LocationCard } from "../../../components/LocationCard/LocationCard"
+import { useContext, useEffect, useState } from "react";
+import { LocationCard } from "../../../components/LocationCard/LocationCard";
 import RickMortyApi from "../../../utils/api/rickMortyApi";
-
+import "../ExplorePage.scss";
+import { SearchInfo } from "../../../components/SearchInfo/SearchInfo";
+import { Filter } from "../../../components/Filter/Filter";
+import { Type } from "../../../components/Filter/categories/locations/Type";
+import useDebounce from "../../../hooks/useDebounce";
+import { Pagination } from "../../../components/Pagination/Pagination";
+import { GalleryContext } from "../../../utils/context/GalleryContext";
 
 export const SearchLocationsPage = () => {
 
-  const [locations, setLocations] = useState([])
-
-  console.log({locations});
-  useEffect(() => {
-    RickMortyApi.getLocations().then((data) => setLocations(data.results))
-    
-  }, []);
-
+  const {infoLocations, locations} = useContext(GalleryContext)
 
   return (
-    <div>
-    {locations.map((location) => <LocationCard location={location} />)}
-       
+    <>
+
+    <div className="search_page__flex_container">
+    <Filter>
+    <Type />
+    </Filter>
+      <div className="search_page__cards_info">
+      <SearchInfo />
+        <div className="search_page_cards__container">
+          {locations.map((location) => (
+            <LocationCard location={location} />
+          ))}
+        </div>
+      </div>
     </div>
-  )
-}
+      <Pagination infoLocations={infoLocations} />
+    </>
+  );
+};
