440f31ecc9d93162659e8ede9d6e01624b90a385
viktoriamay <alterkelly@mail.ru>
2023-04-30 01:41:32 +0300

    Add LocationCard

--- src/components/LocationCard/LocationCard.jsx
+++ src/components/LocationCard/LocationCard.jsx
@@ -1,9 +1,28 @@
+import { Link } from "react-router-dom";
+import './LocationCard.scss';
 
-
-export const LocationCard = ({location}) => {
+export const LocationCard = ({ location }) => {
   return (
-    <div>
-       Location Name {location.name}
-    </div>
-  )
-}
+    <Link
+      to={`/rick-and-morty-gallery/location/${location.id}`}
+      className="location_card"
+    >
+      <div className="location_card__img_container">
+        <img className="location_card__img"
+          src="https://overmental.com/wp-content/uploads/2015/10/rick-and-morty-calaxia.jpg"
+          alt=""
+        />
+        <div className="location_card__name_wrapper">
+
+      <div className="location_card__name">{location.name}</div>
+        </div>
+      </div>
+      <div className="location_card__info_wrapper">
+      <div className="location_card__info">Type: {location.type}</div>
+      <div className="location_card__info">Dimension: {location.dimension}</div>
+      <div className="location_card__info">Residents: {location.residents.length}</div>
+
+      </div>
+    </Link>
+  );
+};
