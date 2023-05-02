108982a75006caf550705e2451017a868e7d0781
viktoriamay <alterkelly@mail.ru>
2023-05-02 21:52:50 +0300

    Edit SearchEpisodesPage

--- src/pages/ExplorePage/SearchEpisodesPage/SearchEpisodesPage.jsx
+++ src/pages/ExplorePage/SearchEpisodesPage/SearchEpisodesPage.jsx
@@ -1,12 +1,40 @@
-import { Pagination } from "antd"
-
+import { useContext } from "react";
+import { GalleryContext } from "../../../utils/context/GalleryContext";
+import { EpisodeCard } from "../../../components/EpisodeCard/EpisodeCard";
+import { Filter } from "../../../components/Filter/Filter";
+import { SearchInfo } from "../../../components/SearchInfo/SearchInfo";
+import { Pagination } from "../../../components/Pagination/Pagination";
+import {
+  Episode,
+  Season1,
+} from "../../../components/Filter/categories/episodes/Season1";
+import { Season2 } from "../../../components/Filter/categories/episodes/Season2";
+import { Season3 } from "../../../components/Filter/categories/episodes/Season3";
+import { Season4 } from "../../../components/Filter/categories/episodes/Season4";
+import { Season5 } from "../../../components/Filter/categories/episodes/Season5";
 
 export const SearchEpisodesPage = () => {
+  const { episodes } = useContext(GalleryContext);
   return (
-    <div style={{backgroundColor: 
-    "white"}}>
-       SearchEpisodesPage
-       <Pagination defaultCurrent={6} total={500} fontSize='30px' />
-    </div>
-  )
-}
+    <>
+      <div className="search_page__flex_container">
+        <Filter>
+          <Season1 />
+          {/* <Season2 /> */}
+          {/* <Season3 /> */}
+          {/* <Season4 /> */}
+          {/* <Season5 /> */}
+        </Filter>
+        <div className="search_page__cards_info">
+          <SearchInfo />
+          <div className="search_page_cards__container">
+            {episodes.map((episode) => (
+              <EpisodeCard episode={episode} />
+            ))}
+          </div>
+        </div>
+      </div>
+      <Pagination />
+    </>
+  );
+};
