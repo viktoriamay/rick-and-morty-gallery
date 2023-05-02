ebc07b589a176942851bab04274be17c90c550f2
viktoriamay <alterkelly@mail.ru>
2023-05-02 21:53:28 +0300

    Add API episodes in CharacterPage

--- src/pages/CharacterPage/CharacterPage.jsx
+++ src/pages/CharacterPage/CharacterPage.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react"
 import RickMortyApi from "../../utils/api/rickMortyApi";
 import { useParams } from "react-router-dom";
 import './CharacterPage.scss';
+import { EpisodeCard } from "../../components/EpisodeCard/EpisodeCard";
 
 
 export const CharacterPage = () => {
@@ -17,6 +18,27 @@ console.log({params});
     })
   }, [params.characterID]);
 
+  const [episodes, setEpisodes] = useState([])
+
+  useEffect(() => {
+    (async function () {
+      if (characterPageInfo?.episode) {
+        // добавляем проверку
+        await Promise.all(
+          characterPageInfo.episode.map((episode) => {
+            return fetch(episode)
+              .then((res) => res.json())
+              .then((res) =>
+              setEpisodes((prevEpisodes) => [...prevEpisodes, res]) 
+              );
+          })
+        );
+      }
+      
+    })();
+  }, [characterPageInfo.episode]);
+
+
   return (
     <div>
        Character number {params.characterID}
@@ -28,8 +50,9 @@ console.log({params});
        <p>{characterPageInfo?.species}</p>
        <p>{characterPageInfo?.created}</p>
        <p>{characterPageInfo?.location?.name}</p>
-       {characterPageInfo?.episode?.map((ep) => <p>{ep}</p>)}
 
+       {/* {characterPageInfo?.episode?.map((ep) => <p>{ep}</p>)} */}
+{episodes.map((episode) => <div>{episode.name}</div>)}
 
     </div>
   )
