92b1896d1fc25526e2f7b257536f4a2b5978618f
viktoriamay <alterkelly@mail.ru>
2023-05-04 19:52:22 +0300

    Add CharacterPage

--- src/pages/CharacterPage/CharacterPage.jsx
+++ src/pages/CharacterPage/CharacterPage.jsx
@@ -1,24 +1,21 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState } from "react";
 import RickMortyApi from "../../utils/api/rickMortyApi";
-import { useParams } from "react-router-dom";
-import './CharacterPage.scss';
-import { EpisodeCard } from "../../components/EpisodeCard/EpisodeCard";
-
+import { Link, useParams } from "react-router-dom";
+import "./CharacterPage.scss";
+import { Accordion } from "../../components/Accordion/Accordion";
 
 export const CharacterPage = () => {
-
   const [characterPageInfo, setCharacterPageInfo] = useState({});
-  console.log({characterPageInfo});
-  const params = useParams()
-console.log({params});
+  const params = useParams();
 
   useEffect(() => {
     RickMortyApi.getCharacterByID(params.characterID).then((data) => {
       setCharacterPageInfo(data);
-    })
+    });
   }, [params.characterID]);
 
-  const [episodes, setEpisodes] = useState([])
+  const [episodes, setEpisodes] = useState([]);
+  const [location, setLocation] = useState({});
 
   useEffect(() => {
     (async function () {
@@ -29,31 +26,84 @@ console.log({params});
             return fetch(episode)
               .then((res) => res.json())
               .then((res) =>
-              setEpisodes((prevEpisodes) => [...prevEpisodes, res]) 
+                setEpisodes((prevEpisodes) => [...prevEpisodes, res])
               );
           })
         );
       }
-      
     })();
   }, [characterPageInfo.episode]);
 
+  useEffect(() => {
+    if (characterPageInfo?.location?.url) {
+      fetch(characterPageInfo.location.url)
+        .then((res) => res.json())
+        .then((res) => setLocation(res));
+    }
+  }, [characterPageInfo.location]);
 
   return (
-    <div>
-       Character number {params.characterID}
+    <div className="character_page">
+      <div className="container">
+        <p className="character_page__title">
+          Профиль персонажа #{characterPageInfo?.id}
+        </p>
+        <div className="character_page__wrapper">
+          <div className="character_page__img">
+            <img
+              src={characterPageInfo?.image}
+              alt={`Avatar of ${characterPageInfo?.name}`}
+            />
+          </div>
+          <div className="character_page__info_wrapper">
+            <div className="character_page__info_item">
+              <p className="character_page__info_data">Name </p>
+              <p className="character_page__name">{characterPageInfo?.name}</p>
+            </div>
+            <div className="character_page__info_item">
+              <p className="character_page__info_data">Gender </p>
+              <p className="character_page__info">
+                {characterPageInfo?.gender}
+              </p>
+            </div>
 
-       <img src={characterPageInfo?.image} alt="" />
-       <h1>{characterPageInfo?.name}</h1>
-       <p>{characterPageInfo?.gender}</p>
-       <p>{characterPageInfo?.status}</p>
-       <p>{characterPageInfo?.species}</p>
-       <p>{characterPageInfo?.created}</p>
-       <p>{characterPageInfo?.location?.name}</p>
-
-       {/* {characterPageInfo?.episode?.map((ep) => <p>{ep}</p>)} */}
-{episodes.map((episode) => <div>{episode.name}</div>)}
-
+            <div className="character_page__info_item">
+              <p className="character_page__info_data">Status </p>
+              <p className="character_page__info">
+                {characterPageInfo?.status}
+              </p>
+            </div>
+            <div className="character_page__info_item">
+              <p className="character_page__info_data">Species </p>
+              <p className="character_page__info">
+                {characterPageInfo?.species}
+              </p>
+            </div>
+            <div className="character_page__info_item">
+              <p className="character_page__info_data">Created at </p>
+              <p className="character_page__info">
+                {new Date(characterPageInfo?.created).toLocaleString("ru-ru")}
+              </p>
+            </div>
+            <div className="character_page__info_item">
+              <p className="character_page__info_data">Last location </p>
+              <Link
+                to={`/rick-and-morty-gallery/location/${location.id}`}
+                className="character_page__info character_page__info_link"
+              >
+                {location.name}
+              </Link>
+            </div>
+          </div>
+        </div>
+        <Accordion title="Episodes">
+          {/* {episodes.map((episode) => (
+            <Link to={`/rick-and-morty-gallery/episode/${episode.id}`}>
+              {episode.name} #{episode.id}
+            </Link>
+          ))} */}
+        </Accordion>
+      </div>
     </div>
-  )
-}
+  );
+};
