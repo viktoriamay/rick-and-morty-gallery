867609a0e218615b4cb361939f08e79221b6a24e
viktoriamay <alterkelly@mail.ru>
2023-05-07 02:04:51 +0300

    Add list in NavLink in ExplorePage

--- src/pages/ExplorePage/ExplorePage.jsx
+++ src/pages/ExplorePage/ExplorePage.jsx
@@ -1,54 +1,71 @@
-import {
-  Link,
-  NavLink,
-  Outlet,
-  Route,
-  Routes,
-  useLocation,
-} from "react-router-dom";
+import { NavLink, Outlet } from "react-router-dom";
 import "./ExplorePage.scss";
 import { Search } from "../../components/Search/Search";
 import { useContext } from "react";
 import { GalleryContext } from "../../utils/context/GalleryContext";
 
 export const ExplorePage = () => {
-  
-  const {setSearchQueryCharacters, setSearchQueryLocations, updatePageNumberLocations, updatePageNumberCharacters} = useContext(GalleryContext)
+  const {
+    setSearchQueryCharacters,
+    setSearchQueryLocations,
+    setSearchQueryEpisodes,
+    updatePageNumberLocations,
+    updatePageNumberCharacters,
+    updatePageNumberEpisodes,
+    updateStatus,
+    updateGender,
+    updateSpecies,
+    updateEpisode,
+    updateType,
+  } = useContext(GalleryContext);
 
   const clearQuery = () => {
-    setSearchQueryLocations('')
-    setSearchQueryCharacters('')
-  }
+    setSearchQueryLocations("");
+    setSearchQueryCharacters("");
+    setSearchQueryEpisodes("");
+    updatePageNumberLocations(1);
+    updatePageNumberCharacters(1);
+    updatePageNumberEpisodes(1);
+    updateStatus("");
+    updateGender("");
+    updateSpecies("");
+    updateEpisode("");
+    updateType("");
+  };
 
   return (
     <div className="explore_page">
       <div className="container">
         <div className="explore_page__container">
-          <div className="explore_page__navlink_container">
-            <NavLink
-              className="explore_page__navlink"
-              to={"/rick-and-morty-gallery/explore/characters"}
-              onClick={clearQuery}
-            >
-              Персонажи
-            </NavLink>
-            <NavLink
-              className="explore_page__navlink"
-              to={"/rick-and-morty-gallery/explore/locations"}
-              onClick={clearQuery}
-
-            >
-              Локации
-            </NavLink>
-            <NavLink
-              className="explore_page__navlink"
-              to={"/rick-and-morty-gallery/explore/episodes"}
-              onClick={clearQuery}
-
-            >
-              Эпизоды
-            </NavLink>
-          </div>
+          <ul className="explore_page__navlink_container">
+            <li>
+              <NavLink
+                className="explore_page__navlink"
+                to={"/rick-and-morty-gallery/explore/characters"}
+                onClick={clearQuery}
+              >
+                Персонажи
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                className="explore_page__navlink"
+                to={"/rick-and-morty-gallery/explore/locations"}
+                onClick={clearQuery}
+              >
+                Локации
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                className="explore_page__navlink"
+                to={"/rick-and-morty-gallery/explore/episodes"}
+                onClick={clearQuery}
+              >
+                Эпизоды
+              </NavLink>
+            </li>
+          </ul>
           <Search />
         </div>
         <Outlet />
