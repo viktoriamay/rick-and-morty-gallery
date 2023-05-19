e9ab3dd536ad41a594de1b140696c73b681af941
viktoriamay <alterkelly@mail.ru>
2023-05-19 23:36:34 +0300

    Add i18n settings and json translations

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import "./index.scss";
 import App from "../src/components/App/App";
 import { BrowserRouter } from "react-router-dom";
-
+import './components/App/i18n'
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
