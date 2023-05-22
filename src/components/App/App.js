408a48da43df5ca9d092bc49fd5793a1d1dfdf06
viktoriamay <alterkelly@mail.ru>
2023-05-22 22:50:15 +0300

    Refactor App

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,33 +1,34 @@
-import { Navigate, Route, Routes } from "react-router-dom";
-import { MainPage } from "../../pages/MainPage/MainPage";
-import { Footer } from "../Footer/Footer";
-import { Header } from "../Header/Header";
-import "./App.scss";
-import { StatisticsPage } from "../../pages/StatisticsPage/StatisticsPage";
-import { ExplorePage } from "./../../pages/ExplorePage/ExplorePage";
-import { NewsPage } from "../../pages/NewsPage/NewsPage";
-import { SearchCharactersPage } from "../../pages/ExplorePage/SearchCharactersPage/SearchCharactersPage";
-import { SearchLocationsPage } from "../../pages/ExplorePage/SearchLocationsPage/SearchLocationsPage";
-import { SearchEpisodesPage } from "../../pages/ExplorePage/SearchEpisodesPage/SearchEpisodesPage";
-import { useEffect, useState } from "react";
-import RickMortyApi from "../../utils/api/rickMortyApi";
-import { GalleryContext } from "../../utils/context/GalleryContext";
-import useDebounce from "../../hooks/useDebounce";
-import { CharacterPage } from "../../pages/ResourcesPages/CharacterPage/CharacterPage";
-import { LocationPage } from "../../pages/ResourcesPages/LocationPage/LocationPage";
-import { EpisodePage } from "../../pages/ResourcesPages/EpisodePage/EpisodePage";
-import { ArticlePage } from "../../pages/ArticlePage/ArticlePage";
-import { useTranslation } from "react-i18next";
-import { AboutProject } from "../../pages/AboutProject/AboutProject";
+import { Navigate, Route, Routes } from 'react-router-dom';
+import { MainPage } from '../../pages/MainPage/MainPage';
+import { Footer } from '../Footer/Footer';
+import { Header } from '../Header/Header';
+import './App.scss';
+import { StatisticsPage } from '../../pages/StatisticsPage/StatisticsPage';
+import { ExplorePage } from './../../pages/ExplorePage/ExplorePage';
+import { NewsPage } from '../../pages/NewsPage/NewsPage';
+import { SearchCharactersPage } from '../../pages/ExplorePage/SearchCharactersPage/SearchCharactersPage';
+import { SearchLocationsPage } from '../../pages/ExplorePage/SearchLocationsPage/SearchLocationsPage';
+import { SearchEpisodesPage } from '../../pages/ExplorePage/SearchEpisodesPage/SearchEpisodesPage';
+import { useEffect, useState } from 'react';
+import RickMortyApi from '../../utils/api/rickMortyApi';
+import { GalleryContext } from '../../utils/context/GalleryContext';
+import useDebounce from '../../hooks/useDebounce';
+import { CharacterPage } from '../../pages/ResourcesPages/CharacterPage/CharacterPage';
+import { LocationPage } from '../../pages/ResourcesPages/LocationPage/LocationPage';
+import { EpisodePage } from '../../pages/ResourcesPages/EpisodePage/EpisodePage';
+import { ArticlePage } from '../../pages/ArticlePage/ArticlePage';
+import { useTranslation } from 'react-i18next';
+import { AboutProject } from '../../pages/AboutProject/AboutProject';
+import { AboutRM } from '../../pages/AboutRM/AboutRM';
 
 function App() {
   const [characters, setCharacters] = useState([]);
   const [infoCharacters, setInfoCharacters] = useState({});
   const [pageNumberCharacters, updatePageNumberCharacters] = useState(1);
-  const [searchQueryCharacters, setSearchQueryCharacters] = useState("");
-  const [status, updateStatus] = useState("");
-  const [gender, updateGender] = useState("");
-  const [species, updateSpecies] = useState("");
+  const [searchQueryCharacters, setSearchQueryCharacters] = useState('');
+  const [status, updateStatus] = useState('');
+  const [gender, updateGender] = useState('');
+  const [species, updateSpecies] = useState('');
 
   const debounceSearchQueryCharacters = useDebounce(
     searchQueryCharacters,
@@ -38,9 +39,9 @@ function App() {
 
   const [locations, setLocations] = useState([]);
   const [infoLocations, setInfoLocations] = useState({});
-  const [type, updateType] = useState("");
-  const [dimension, updateDimension] = useState("");
-  const [searchQueryLocations, setSearchQueryLocations] = useState("");
+  const [type, updateType] = useState('');
+  const [dimension, updateDimension] = useState('');
+  const [searchQueryLocations, setSearchQueryLocations] = useState('');
 
   const [pageNumberLocations, updatePageNumberLocations] = useState(1);
 
@@ -59,7 +60,7 @@ function App() {
       });
   }, [debounceSearchQueryLocations, dimension, pageNumberLocations, type]);
   /////////
-  const [searchQueryEpisodes, setSearchQueryEpisodes] = useState("");
+  const [searchQueryEpisodes, setSearchQueryEpisodes] = useState('');
 
   const debounceSearchQueryEpisodes = useDebounce(searchQueryEpisodes, 1000);
 
@@ -106,28 +107,39 @@ function App() {
       });
   }, [debounceSearchQueryEpisodes, episode, pageNumberEpisodes]);
 
-  const [theme, setTheme] = useState(localStorage.getItem("theme"));
-  
+  const [theme, setTheme] = useState(localStorage.getItem('theme') ?? 'dark');
+
   const handleThemeChange = () => {
-    setTheme(theme === "dark" ? "light" : "dark");
+    setTheme(theme === 'dark' ? 'light' : 'dark');
   };
 
   useEffect(() => {
-    localStorage.setItem("theme", theme);
+    localStorage.setItem('theme', theme);
   }, [theme]);
-
 
   const { t, i18n } = useTranslation();
 
-  const [lang, setLang] = useState("ru");
+  // const [lang, setLang] = useState('ru');
+  const [lang, setLang] = useState(localStorage.getItem('lang') ?? 'ru');
 
   const changeLanguage = () => {
-    const lang = localStorage.getItem("lang") ?? "ru";
-    const newLang = lang === "ru" ? "en" : "ru";
+    // const lang = localStorage.getItem('lang') ?? 'ru';
+    const newLang = lang === 'ru' ? 'en' : 'ru'
     i18n.changeLanguage(newLang);
-    setLang(newLang);
-    localStorage.setItem("lang", newLang);
-  };
+    setLang(newLang)
+    // localStorage.setItem('lang', newLang);
+  }
+  // const changeLanguage = () => {
+  //   // const lang = localStorage.getItem('lang') ?? 'ru';
+  //   const newLang = lang === 'ru' ? 'en' : 'ru'
+  //   i18n.changeLanguage(newLang);
+  //   setLang(newLang)
+  //   localStorage.setItem('lang', newLang);
+  // }
+
+  useEffect(() => {
+    localStorage.setItem('lang', lang);
+  }, [lang]);
 
   const valueContextProvider = {
     episode,
@@ -199,6 +211,7 @@ function App() {
             <Route path="/news" element={<NewsPage />} />
             <Route path="/news/:articleID" element={<ArticlePage />} />
             <Route path="/about-project" element={<AboutProject />} />
+            <Route path="/about-rick-and-morty" element={<AboutRM />} />
           </Routes>
         </main>
         <Footer />
