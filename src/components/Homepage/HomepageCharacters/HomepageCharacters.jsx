9ef538206571e7df48c6c899f5b01f646bb42211
viktoriamay <alterkelly@mail.ru>
2023-05-23 23:02:27 +0300

    Add to HomepageCharacters responsive version

--- src/components/Homepage/HomepageCharacters/HomepageCharacters.jsx
+++ src/components/Homepage/HomepageCharacters/HomepageCharacters.jsx
@@ -1,19 +1,52 @@
-import { useContext } from "react";
-import { CharacterCard } from "../../Cards/CharacterCard";
-import "./HomepageCharacters.scss";
-import { GalleryContext } from "../../../utils/context/GalleryContext";
+import { useContext } from 'react';
+import { CharacterCard } from '../../Cards/CharacterCard';
+import { GalleryContext } from '../../../utils/context/GalleryContext';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import './HomepageCharacters.scss';
+import 'swiper/scss';
 
 export const HomepageCharacters = () => {
+  const { characters, t } = useContext(GalleryContext);
 
-  const {characters, t} = useContext(GalleryContext)
+  const firstFive = characters.slice(0, 5);
+  const secondFive = characters.slice(0, 5);
+  const firstTen = firstFive.concat(secondFive).slice(0, 10);
+
   return (
-    <div className="container">
-      <div className="homepage_characters">
-        <h2 className="homepage__title">{t('mainCharacters')}</h2>
-        <div className="homepage_characters__cards">
-        {characters.map((character) => <CharacterCard character={character} key={character.id} />).slice(0, 5)}
-        </div>
-      </div>
-    </div>
+    <section className="homepage_characters">
+      <h2 className="homepage__title container">{t('mainCharacters')}</h2>
+      <Swiper
+        slidesPerView={4}
+        spaceBetween={20}
+        loop={true}
+        breakpoints={{
+          310: {
+            slidesPerView: 1,
+            spaceBetween: 20,
+          },
+          500: {
+            slidesPerView: 2,
+            spaceBetween: 20,
+          },
+          768: {
+            slidesPerView: 3,
+            spaceBetween: 20,
+          },
+          1024: {
+            slidesPerView: 4,
+            spaceBetween: 30,
+          },
+        }}
+      >
+        {firstTen.map((character, i) => (
+          <SwiperSlide
+            className="swiper-slide"
+            key={`main-character-${character.id}-${i}`}
+          >
+            <CharacterCard character={character} />
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </section>
   );
 };
