92d7287d446506adb7c3e3dd41b0675eb8899971
viktoriamay <alterkelly@mail.ru>
2023-05-31 22:07:16 +0300

    Refactor CharacterCard

--- src/components/Cards/CharacterCard.jsx
+++ src/components/Cards/CharacterCard.jsx
@@ -1,33 +1,21 @@
-import { GrUserManager } from "react-icons/gr";
-import { RiAliensLine } from "react-icons/ri";
-
-import "./Card.scss";
-import { Link } from "react-router-dom";
-import { useContext } from "react";
-import { GalleryContext } from "../../utils/context/GalleryContext";
+import './Card.scss';
+import { useContext } from 'react';
+import { GalleryContext } from '../../utils/context/GalleryContext';
+import { Link } from 'react-router-dom';
 
 export const CharacterCard = ({ character }) => {
+  const { theme } = useContext(GalleryContext);
 
-  const {theme} = useContext(GalleryContext)
-  const getSpeciesIcon = (species) => {
-    switch (species) {
-      case "Human":
-        return <GrUserManager />;
-      case "Alien":
-        return <RiAliensLine />;
-      default:
-        return null;
-    }
-  };
   const characterStatus = (status) => {
     switch (status) {
-      case "Alive":
-        return "card__status_alive"
-      case "Dead":
-                return "card__status_dead"
+      case 'Alive':
+        return 'card__status_alive';
 
-      case "unknown":
-                return "card__status_unknown"
+      case 'Dead':
+        return 'card__status_dead';
+
+      case 'unknown':
+        return 'card__status_unknown';
 
       default:
         return null;
@@ -35,28 +23,34 @@ export const CharacterCard = ({ character }) => {
   };
 
   const parameter = (param, name) => {
-    return param === 'unknown' ? param.replace('unknown', 'Unknown') + (name) : param;
-  }
+    return param === 'unknown'
+      ? param.replace('unknown', 'Unknown') + name
+      : param;
+  };
 
   return (
-    <Link
-      to={`/character/${character?.id}`}
-      className={`card ${theme}`}
-    >
+    <Link to={`/character/${character?.id}`} className={`card ${theme}`}>
       <div className="card__img_wrapper">
         <img
           src={character?.image}
           alt={`${character?.name}'s Avatar`}
           className="card__img"
         />
-        <div className={`card__status ${characterStatus(character?.status)}`}>{character?.status}</div>
+        <div className={`card__status ${characterStatus(character?.status)}`}>
+          {character?.status}
+        </div>
       </div>
       <div className="card__info_wrapper">
         <p className="card__name">{character?.name}</p>
-        <p className={`card__info ${theme}`}>{parameter(character?.species, ' Species')}
+        <p className={`card__info ${theme}`}>
+          {parameter(character?.species, ' Species')}
         </p>
-        <p className={`card__info ${theme}`}>{parameter(character?.gender, ' Gender')}</p>
-        <p className={`card__info ${theme}`}>{parameter(character?.location?.name, ' Dimension') }</p>
+        <p className={`card__info ${theme}`}>
+          {parameter(character?.gender, ' Gender')}
+        </p>
+        <p className={`card__info ${theme}`}>
+          {parameter(character?.location?.name, ' Dimension')}
+        </p>
       </div>
     </Link>
   );
