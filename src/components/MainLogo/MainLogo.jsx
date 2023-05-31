c575dfb70d11efb54b283f25efe663c068db1496
viktoriamay <alterkelly@mail.ru>
2023-06-01 01:17:07 +0300

    Refactor MainLogo

--- src/components/MainLogo/MainLogo.jsx
+++ src/components/MainLogo/MainLogo.jsx
@@ -1,11 +1,26 @@
-import { Link } from "react-router-dom";
-import Logo from "./img/Rick_and_Morty.png";
-import "./MainLogo.scss";
+import './MainLogo.scss';
+import { Link } from 'react-router-dom';
+import { useContext } from 'react';
+import { GalleryContext } from '../../utils/context/GalleryContext';
+import Logo from './img/Rick_and_Morty.png';
 
 export const MainLogo = () => {
+  const {
+    updatePageNumberCharacters,
+    updateStatus,
+    updateGender,
+    updateSpecies,
+  } = useContext(GalleryContext);
+
+  const clearCharacters = () => {
+    updatePageNumberCharacters(1);
+    updateStatus('');
+    updateGender('');
+    updateSpecies('');
+  };
   return (
-    <Link to="/">
-      <img className="main__logo" src={Logo} alt="rick-and-morty-logo" />
+    <Link to="/" onClick={() => clearCharacters()}>
+      <img className="main__logo" src={Logo} alt="Rick and Morty Logo" />
     </Link>
   );
 };
