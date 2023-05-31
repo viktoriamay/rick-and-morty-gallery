76933b3b19b4b9e6c7b7f6dc11fc88f0bdf135bd
viktoriamay <alterkelly@mail.ru>
2023-05-31 23:48:48 +0300

    Refactor Filter component

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,6 +1,6 @@
-import { useContext, useEffect } from "react";
-import "./Filter.scss";
-import { GalleryContext } from "../../utils/context/GalleryContext";
+import './Filter.scss';
+import { useContext } from 'react';
+import { GalleryContext } from '../../utils/context/GalleryContext';
 
 export const Filter = ({ children }) => {
   const {
@@ -10,35 +10,36 @@ export const Filter = ({ children }) => {
     updateStatus,
     updateGender,
     updateSpecies,
-    updateType, updateEpisode, t
+    updateType,
+    updateEpisode,
+    t,
   } = useContext(GalleryContext);
 
-  const radios = document.querySelectorAll('input[type="radio"]');
-  const clearF = () => {
-    radios.forEach((rado) => /* console.log */ (rado.checked = false));
+  const radioButtons = document.querySelectorAll('input[type="radio"]');
+
+  const uncheckRadioButtons = () => {
+    radioButtons.forEach((radioButton) => (radioButton.checked = false));
   };
 
   const clearFilters = () => {
-    updateStatus("");
-    updateGender("");
-    updateSpecies("");
-    updateType("");
-    updateEpisode("")
+    updateStatus('');
+    updateGender('');
+    updateSpecies('');
+    updateType('');
+    updateEpisode('');
     updatePageNumberCharacters(1);
     updatePageNumberLocations(1);
     updatePageNumberEpisodes(1);
-    clearF();
+    uncheckRadioButtons();
   };
 
   return (
     <div>
       <h3 className="filter_form__title">{t('filters')}</h3>
       <div className="filter_form__clear_btn" onClick={clearFilters}>
-      {t('clearFilters')}
+        {t('clearFilters')}
       </div>
-      <div className="filter_form__inputs">
-        {children}
-      </div>
+      <div className="filter_form__inputs">{children}</div>
     </div>
   );
 };
--- src/components/Filter/FilterOptionBTN.jsx
+++ src/components/Filter/FilterOptionBTN.jsx
@@ -1,19 +1,37 @@
-import "./Filter.scss"
+import './Filter.scss';
 
-export const FilterOptionBTN = ({ optionName, task, updatePageNumber, index, name,updateStatus, status, option,checked, updateSpecies, updateGender }) => {
- 
-
+export const FilterOptionBTN = ({
+  optionName,
+  task,
+  index,
+  name,
+  checked,
+  updateSpecies,
+  updateGender,
+}) => {
   return (
-// делаем ОДНУ радиокнопку, которую мапим в компонентах статус/вид и тд
-// нейм это имя компонента в котором используем кнопку, то есть присваиваем инпуты к определенной категории
-// айди это имя (статус например) и номер в массиве (0, 1, 2)
-// фор нужно для того чтобы при нажатии на опшонНейм выбирался соответствующий инпут, он соответствует имени+айди
+    // делаем ОДНУ радиокнопку, которую мапим в компонентах статус/вид и тд
+    // нейм это имя компонента в котором используем кнопку, то есть присваиваем инпуты к определенной категории
+    // айди это имя (статус например) и номер в массиве (0, 1, 2)
+    // фор нужно для того чтобы при нажатии на опшонНейм выбирался соответствующий инпут, он соответствует имени+айди
 
-<div className="option_btn__wrapper">
-  <input type="radio" checked={checked}  name={name} id={`${name}-${index}`} className="option_btn__radio"  />
-  <label onClick={() => {
-      task(optionName, updateSpecies, updateGender)
-    }} htmlFor={`${name}-${index}`} className="option_btn__label">{optionName}</label>
-</div>
+    <div className="option_btn__wrapper">
+      <input
+        type="radio"
+        checked={checked}
+        name={name}
+        id={`${name}-${index}`}
+        className="option_btn__radio"
+      />
+      <label
+        onClick={() => {
+          task(optionName, updateSpecies, updateGender);
+        }}
+        htmlFor={`${name}-${index}`}
+        className="option_btn__label"
+      >
+        {optionName}
+      </label>
+    </div>
   );
 };
