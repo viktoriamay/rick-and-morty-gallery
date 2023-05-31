71598de5e4a57b1ba5d134560c43808bbcb1b6d8
viktoriamay <alterkelly@mail.ru>
2023-05-31 22:34:19 +0300

    Refactor NewsCardHomePage

--- src/components/Cards/NewsCardHomePage.jsx
+++ src/components/Cards/NewsCardHomePage.jsx
@@ -1,33 +1,23 @@
-import { Link } from 'react-router-dom';
 import './Card.scss';
+import { Link } from 'react-router-dom';
 import { useContext } from 'react';
 import { GalleryContext } from '../../utils/context/GalleryContext';
 
-export const NewsCardHomePage = ({data}) => {
-  const {theme, t} = useContext(GalleryContext)
+export const NewsCardHomePage = ({ customNews }) => {
+  const { theme, t } = useContext(GalleryContext);
 
   return (
-    <>
-    <Link to={`/news`} 
-    className={`homepage_news__card ${theme}`}
-    >
-
-    <div className="card__img_wrapper">
-      <img
-        src={data?.image}
-        alt=""
-        className="card__img"
-      />
-    </div>
-    <div className="card__info_wrapper card__info__news_wrapper">
-      <p className="card__name card__name_location">
-        {data?.title}
-      </p>
-      <span className={`news__card_text ${theme}`}>{data?.summary}</span>
-      <span className="news__card_button">{t('newsCardButton')}</span>
-    </div>
-  </Link>
-  </>
-
-  )
-}
+    <Link to={`/news`} className={`homepage_news__card ${theme}`}>
+      <div className="card__img_wrapper">
+        <img src={customNews?.image} alt="Rick and Morty News Cover" className="card__img" />
+      </div>
+      <div className="card__info_wrapper card__info__news_wrapper">
+        <p className="card__name card__name_location">{customNews?.title}</p>
+        <span className={`news__card_text ${theme}`}>
+          {customNews?.summary}
+        </span>
+        <span className="news__card_button">{t('newsCardButton')}</span>
+      </div>
+    </Link>
+  );
+};
