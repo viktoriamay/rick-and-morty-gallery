b3e532c9cff605a2e453dbe4e039b1c98c125b7e
viktoriamay <alterkelly@mail.ru>
2023-05-31 22:02:33 +0300

    Refactor Accordion

--- src/components/Accordion/Accordion.jsx
+++ src/components/Accordion/Accordion.jsx
@@ -1,24 +1,24 @@
-import { useContext, useState } from "react";
-import './Accordion.scss'
-import { GalleryContext } from "../../utils/context/GalleryContext";
+import './Accordion.scss';
+import { useContext, useState } from 'react';
+import { GalleryContext } from '../../utils/context/GalleryContext';
 
-export const Accordion = ({title, children}) => {
-
-  const {theme} = useContext(GalleryContext);
+export const Accordion = ({ title, children }) => {
+  const { theme } = useContext(GalleryContext);
 
   const [selected, setSelected] = useState(false);
 
   const toggleState = () => {
-    setSelected(selected => !selected);
-  }
+    setSelected((selected) => !selected);
+  };
+
   return (
     <>
-      <div className={selected ? "accordion active" : "accordion"}>
+      <div className={selected ? 'accordion active' : 'accordion'}>
         <button className={`accordion__button ${theme}`} onClick={toggleState}>
           <h3 className={`accordion__title ${theme}`}>{title}</h3>
         </button>
-        <div className={ `accordion__content ${theme}`}>
-          <div className={"accordion__text"}>{children}</div>
+        <div className={`accordion__content ${theme}`}>
+          <div className={'accordion__text'}>{children}</div>
         </div>
       </div>
     </>
