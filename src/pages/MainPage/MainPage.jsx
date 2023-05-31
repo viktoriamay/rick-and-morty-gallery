d590e9eef5ff16565cd429cf04a25c52938d9b5c
viktoriamay <alterkelly@mail.ru>
2023-06-01 02:17:53 +0300

    Refactor Main and NotFound Pages

--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -1,8 +1,8 @@
-import { HomepageCategories } from "../../components/Homepage/HomepageCategories/HomepageCategories";
-import { HomepageCharacters } from "../../components/Homepage/HomepageCharacters/HomepageCharacters";
-import { HomepageInfo } from "../../components/Homepage/HomepageInfo/HomepageInfo";
-import { HomepageNews } from "../../components/Homepage/HomepageNews/HomepageNews";
-import "./MainPage.scss";
+import './MainPage.scss';
+import { HomepageInfo } from '../../components/Homepage/HomepageInfo/HomepageInfo';
+import { HomepageCategories } from '../../components/Homepage/HomepageCategories/HomepageCategories';
+import { HomepageCharacters } from '../../components/Homepage/HomepageCharacters/HomepageCharacters';
+import { HomepageNews } from '../../components/Homepage/HomepageNews/HomepageNews';
 
 export const MainPage = () => {
   return (
--- src/pages/NotFoundPage/NotFoundPage.jsx
+++ src/pages/NotFoundPage/NotFoundPage.jsx
@@ -1,19 +1,18 @@
-import { Link } from 'react-router-dom';
 import './NotFoundPage.scss';
+import { Link } from 'react-router-dom';
 import { useContext } from 'react';
 import { GalleryContext } from '../../utils/context/GalleryContext';
 
 export const NotFoundPage = () => {
   const { t } = useContext(GalleryContext);
+
   return (
-    <div className="container">
-      <section className="not_found">
-        <div className="not_found__error">404</div>
-        <div className="not_found__text">{t('notFound')}</div>
-        <Link to={'/'} className="homepage_info__button">
-          {t('toMainPageButton')}
-        </Link>
-      </section>
-    </div>
+    <section className="not_found container">
+      <div className="not_found__error">404</div>
+      <div className="not_found__text">{t('notFound')}</div>
+      <Link to={'/'} className="homepage_info__button">
+        {t('toMainPageButton')}
+      </Link>
+    </section>
   );
 };
