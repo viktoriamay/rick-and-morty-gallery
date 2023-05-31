0f77e6244cc3cd01680093bd70e1466a830844bc
viktoriamay <alterkelly@mail.ru>
2023-05-31 21:58:54 +0300

    Refactor BurgerMenu

--- src/components/BurgerMenu/BurgerMenu.jsx
+++ src/components/BurgerMenu/BurgerMenu.jsx
@@ -1,10 +1,21 @@
-import './BurgerMenu.scss'
+import './BurgerMenu.scss';
+import { useContext } from 'react';
+import { GalleryContext } from '../../utils/context/GalleryContext';
 
+export const BurgerMenu = ({ onClick, activeBurger }) => {
+  const { theme } = useContext(GalleryContext);
 
-export const BurgerMenu = ({onClick, activeBurger}) => {
   return (
-    <div className='burger_button' onClick={onClick}>
-       <div className={activeBurger ? "burger_btn active": "burger_btn"}></div>
+    <div className="burger_button" onClick={onClick}>
+      <div
+        className={
+          activeBurger
+            ? 'burger_btn active'
+            : 'burger_btn' && theme === 'light'
+            ? 'burger_btn light'
+            : 'burger_btn'
+        }
+      ></div>
     </div>
-  )
-}
+  );
+};
