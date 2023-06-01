05bf4f070b58fbb10ae8f4e93114be508aea37eb
viktoriamay <alterkelly@mail.ru>
2023-06-01 03:05:50 +0300

    Refactor StatisticsPage

--- src/pages/StatisticsPage/StatisticsPage.jsx
+++ src/pages/StatisticsPage/StatisticsPage.jsx
@@ -1,245 +1,169 @@
-import { useContext, useEffect, useState } from "react";
-import { GalleryContext } from "../../utils/context/GalleryContext";
-import { LocationsTypeChart } from "../../components/Charts/LocationsTypeChart";
-import { CharactersEpisodesChart } from "../../components/Charts/CharactersEpisodesChart";
-import RickMortyApi from "../../utils/api/rickMortyApi";
-import { CharactersStatusChart } from "../../components/Charts/CharactersStatusChart";
-import { BackButton } from "../../components/BackButton/BackButton";
+import { useContext, useEffect, useMemo, useState } from 'react';
+import { GalleryContext } from '../../utils/context/GalleryContext';
+import { LocationsTypeChart } from '../../components/Charts/LocationsTypeChart';
+import { CharactersEpisodesChart } from '../../components/Charts/CharactersEpisodesChart';
+import { CharactersStatusChart } from '../../components/Charts/CharactersStatusChart';
+import RickMortyApi from '../../utils/api/rickMortyApi';
+import { BackButton } from '../../components/BackButton/BackButton';
 
 export const StatisticsPage = () => {
-  // const { characters, infoCharacters, infoLocations, infoEpisodes } =
-  //   useContext(GalleryContext);
-
-  // useEffect(() => {
-  //   let chartDom = document.getElementById("main");
-  //   let myChart = echarts.init(chartDom);
-  //   let option;
-
-  //   option = {
-  //     tooltip: {
-  //       trigger: "item",
-  //     },
-  //     legend: {
-  //       // top: '5%',
-  //       // left: 'center'
-  //       orient: "vertical",
-  //       left: "left",
-  //     },
-  //     series: [
-  //       {
-  //         name: "Access From",
-  //         type: "pie",
-  //         radius: ["40%", "70%"],
-  //         avoidLabelOverlap: false,
-  //         itemStyle: {
-  //           borderRadius: 10,
-  //           borderColor: "#fff",
-  //           borderWidth: 2,
-           
-  //           // color: {
-  //           //   image: "https://echarts.apache.org/en/images/logo.png", // supported as HTMLImageElement, HTMLCanvasElement, but not path string of SVG
-  //           //   repeat: "repeat", // whether to tile, can be 'repeat-x', 'repeat-y', 'no-repeat'
-  //           // },
-  //           colorBy: "data",
-  //         },
-  //         label: {
-  //           show: true,
-  //           position: "outside",
-  //           textBorderWidth: 0,
-  //           // textBorderColor: 'pink'
-  //         },
-  //         emphasis: {
-  //           label: {
-  //             show: true,
-  //             fontSize: 40,
-  //             fontWeight: "bold",
-  //           },
-  //         },
-  //         labelLine: {
-  //           show: true,
-  //           length: 28,
-  //         },
-
-  //         data: [
-  //           {
-  //             value: characters[0]?.episode?.length,
-  //             name: characters[0]?.name,
-  //             itemStyle: {
-  //               // color: '#FF0000' // задаем красный цвет для первого элемента
-  //               color: {
-
-  //                 image: "https://echarts.apache.org/en/images/logo.png", // supported as HTMLImageElement, HTMLCanvasElement, but not path string of SVG
-  //                 repeat: "repeat", // whether to tile, can be 'repeat-x', 'repeat-y', 'no-repeat'
-  //               }
-  //             }
-  //           },
-  //           {
-  //             value: characters[1]?.episode?.length,
-  //             name: characters[1]?.name,
-  //           },
-  //           {
-  //             value: characters[2]?.episode?.length,
-  //             name: characters[2]?.name,
-  //           },
-  //           {
-  //             value: characters[3]?.episode?.length,
-  //             name: characters[3]?.name,
-  //           },
-  //           {
-  //             value: characters[4]?.episode?.length,
-  //             name: characters[4]?.name,
-  //           },
-  //           // { value: infoLocations?.count, name: 'Количество локаций' },
-  //           // { value: infoEpisodes?.count, name: 'Количество эпизодов' },
-  //           // { value: 484, name: 'Union Ads' },
-  //           // { value: 300, name: 'Video Ads' },
-  //           // { value: 300, name: 'Video Ads' }
-  //         ],
-  //       },
-  //     ],
-  //   };
-
-    // option = {
-    //   title: {
-    //     text: 'Referer of a Website',
-    //     subtext: 'Fake Data',
-    //     left: 'center'
-    //   },
-    //   tooltip: {
-    //     trigger: 'item'
-    //   },
-    //   legend: {
-    //     orient: 'vertical',
-    //     left: 'left'
-    //   },
-    //   series: [
-    //     {
-    //       name: 'Access From',
-    //       type: 'pie',
-    //       radius: '50%',
-    //       data: [
-    //         { value: characters[0]?.episode?.length, name: characters[0]?.name },
-    //         { value: characters[1]?.episode?.length, name: characters[1]?.name },
-    //         { value: characters[2]?.episode?.length, name: characters[2]?.name },
-    //         { value: characters[3]?.episode?.length, name: characters[3]?.name },
-    //         { value: characters[4]?.episode?.length, name: characters[4]?.name },
-    //       ],
-    //       emphasis: {
-    //         itemStyle: {
-    //           shadowBlur: 10,
-    //           shadowOffsetX: 0,
-    //           shadowColor: 'rgba(0, 0, 0, 0.5)'
-    //         }
-    //       }
-    //     }
-    //   ]
-    // };
-
-  //   option && myChart.setOption(option);
-  // }, []);
-
-  const {
-    characters,
-    locationTypePlanet,
-    locationTypeSpaceStation,
-    infoCharacters,
-    infoLocations,
-    infoEpisodes,
-    updateType,
-    type,
-    locations, theme, t
-  } = useContext(GalleryContext);
+  const { theme, t } = useContext(GalleryContext);
 
   const [planet, setPlanet] = useState(0);
   const [spaceStation, setSpaceStation] = useState(0);
   const [microverse, setMicroverse] = useState(0);
   const [dimension, setDimension] = useState(0);
   const [dream, setDream] = useState(0);
-
-  useEffect(() => {
-  Promise.all([
-    RickMortyApi.getLocations(1, '', 'Planet', '').then((locations) => {
-      setPlanet(locations?.info?.count);
-    }),
-    RickMortyApi.getLocations(1, '', 'Space station', '').then((locations) => {
-      setSpaceStation(locations?.info?.count);
-    }),
-    RickMortyApi.getLocations(1, '', 'Microverse', '').then((locations) => {
-      setMicroverse(locations?.info?.count);
-    }),
-    RickMortyApi.getLocations(1, '', 'Dimension', '').then((locations) => {
-      setDimension(locations?.info?.count);
-    }),
-    RickMortyApi.getLocations(1, '', 'Dream', '').then((locations) => {
-      setDream(locations?.info?.count);
-    }),
-  ]).then(() => {
-    // Действия после выполнения всех промисов
-  });
-}, [infoLocations.count, type, updateType]);
-
-const [alive, setAlive] = useState(0);
+  const [alive, setAlive] = useState(0);
   const [dead, setDead] = useState(0);
   const [unknown, setUnknown] = useState(0);
+  const [activeTab, setActiveTab] = useState(0);
+
+  const locationsData = useMemo(
+    () =>
+      Promise.all([
+        RickMortyApi.getLocations(1, '', 'Planet', '').then(
+          (locations) => locations?.info?.count
+        ),
+        RickMortyApi.getLocations(1, '', 'Space station', '').then(
+          (locations) => locations?.info?.count
+        ),
+        RickMortyApi.getLocations(1, '', 'Microverse', '').then(
+          (locations) => locations?.info?.count
+        ),
+        RickMortyApi.getLocations(1, '', 'Dimension', '').then(
+          (locations) => locations?.info?.count
+        ),
+        RickMortyApi.getLocations(1, '', 'Dream', '').then(
+          (locations) => locations?.info?.count
+        ),
+      ]),
+    []
+  );
+
+  const charactersData = useMemo(
+    () =>
+      Promise.all([
+        RickMortyApi.getCharacters(1, '', 'Alive', '', '').then(
+          (characters) => characters?.info?.count
+        ),
+        RickMortyApi.getCharacters(1, '', 'Dead', '', '').then(
+          (characters) => characters?.info?.count
+        ),
+        RickMortyApi.getCharacters(1, '', 'unknown', '', '').then(
+          (characters) => characters?.info?.count
+        ),
+      ]),
+    []
+  );
 
   useEffect(() => {
-    Promise.all([
-      RickMortyApi.getCharacters(1, '', 'Alive', '', '').then((characters) => {
-        setAlive(characters?.info?.count);
-      }),
-      RickMortyApi.getCharacters(1, '', 'Dead', '', '').then((characters) => {
-        setDead(characters?.info?.count);
-      }),
-      RickMortyApi.getCharacters(1,  '', 'unknown', '', '').then((characters) => {
-        setUnknown(characters?.info?.count);
-      })
-    ]).then(() => {
-      // Действия после выполнения всех промисов
+    locationsData.then((data) => {
+      setPlanet(data[0]);
+      setSpaceStation(data[1]);
+      setMicroverse(data[2]);
+      setDimension(data[3]);
+      setDream(data[4]);
     });
-  }, [infoCharacters.count, type, updateType]);
-
-
-  const [activeTab, setActiveTab] = useState(0);
+    charactersData.then((data) => {
+      setAlive(data[0]);
+      setDead(data[1]);
+      setUnknown(data[2]);
+    });
+  }, [locationsData, charactersData]);
 
   const handleTabClick = (index) => {
     setActiveTab(index);
   };
 
   const tabs = [
-    { label: t('episodesWithCharacters'), content: <CharactersEpisodesChart /> },
-    { label: t('locationsByType'), content: <LocationsTypeChart planet={planet} spaceStation={spaceStation} microverse={microverse} dimension={dimension} dream={dream} /> },
-    { label: t('charactersStatus'), content: <CharactersStatusChart alive={alive} dead={dead} unknown={unknown} /> },
+    {
+      label: t('episodesWithCharacters'),
+      content: <CharactersEpisodesChart />,
+    },
+    {
+      label: t('locationsByType'),
+      content: (
+        <LocationsTypeChart
+          planet={planet}
+          spaceStation={spaceStation}
+          microverse={microverse}
+          dimension={dimension}
+          dream={dream}
+        />
+      ),
+    },
+    {
+      label: t('charactersStatus'),
+      content: (
+        <CharactersStatusChart alive={alive} dead={dead} unknown={unknown} />
+      ),
+    },
   ];
 
   return (
     <div>
-    <div className="container">
-<BackButton />
-      <ul className="explore_page__navlink_container">
-        {tabs.map((tab, index) => (
-          <li
-            key={index}
-            className={activeTab === index ? `active explore_page__navlink ${theme}`: `explore_page__navlink ${theme}`}
-            onClick={() => handleTabClick(index)}
-          >
-            {tab.label}
-          </li>
-        ))}
-      </ul>
-      <div>{tabs[activeTab].content}</div>
-    </div>
+      <div className="container">
+        <BackButton />
+        <ul className="explore_page__navlink_container">
+          {tabs.map((tab, index) => (
+            <li
+              key={index}
+              className={
+                activeTab === index
+                  ? `active explore_page__navlink ${theme}`
+                  : `explore_page__navlink ${theme}`
+              }
+              onClick={() => handleTabClick(index)}
+            >
+              {tab.label}
+            </li>
+          ))}
+        </ul>
+        <div>{tabs[activeTab].content}</div>
+      </div>
     </div>
   );
-
-
-  // return (
-  //   <div>
-  //   <div className="container">
-
-  //     STATISTICS
-  //     <CharactersEpisodesChart />
-  //     <EpisodesCharactersChart />
-  //   </div>
-  //   </div>
-  // );
 };
+
+// useEffect(() => {
+//   Promise.all([
+//     RickMortyApi.getLocations(1, '', 'Planet', '').then((locations) => {
+//       setPlanet(locations?.info?.count);
+//     }),
+//     RickMortyApi.getLocations(1, '', 'Space station', '').then(
+//       (locations) => {
+//         setSpaceStation(locations?.info?.count);
+//       }
+//     ),
+//     RickMortyApi.getLocations(1, '', 'Microverse', '').then((locations) => {
+//       setMicroverse(locations?.info?.count);
+//     }),
+//     RickMortyApi.getLocations(1, '', 'Dimension', '').then((locations) => {
+//       setDimension(locations?.info?.count);
+//     }),
+//     RickMortyApi.getLocations(1, '', 'Dream', '').then((locations) => {
+//       setDream(locations?.info?.count);
+//     }),
+//   ]).then(() => {
+//     // Действия после выполнения всех промисов
+//   });
+// }, [infoLocations.count, type, updateType]);
+
+// useEffect(() => {
+//   Promise.all([
+//     RickMortyApi.getCharacters(1, '', 'Alive', '', '').then((characters) => {
+//       setAlive(characters?.info?.count);
+//     }),
+//     RickMortyApi.getCharacters(1, '', 'Dead', '', '').then((characters) => {
+//       setDead(characters?.info?.count);
+//     }),
+//     RickMortyApi.getCharacters(1, '', 'unknown', '', '').then(
+//       (characters) => {
+//         setUnknown(characters?.info?.count);
+//       }
+//     ),
+//   ]).then(() => {
+//     // Действия после выполнения всех промисов
+//   });
+// }, [infoCharacters.count, type, updateType]);
